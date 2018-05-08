78ddc68acf6625e2e032a8418c0d5d1a321ca4f8
Brian Lee <leebrian7799@gmail.com>
2018-05-08 19:54:09 -0400

    Set up npm and bundle

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/d_dot.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
