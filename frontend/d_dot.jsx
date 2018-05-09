3789305eda8b3c61b914b0976a92233110e43c34
Brian Lee <leebrian7799@gmail.com>
2018-05-09 14:06:55 -0400

    Add session reducer

--- frontend/d_dot.jsx
+++ frontend/d_dot.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-let store = configureStore;
-let root = document.getElementById('root');
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<h1>Welcom to DDOT</h1>, root);
+  let root = document.getElementById('root');
+  ReactDOM.render(<Root />, root);
 });
