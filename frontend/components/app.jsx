808daf80846b42cd0022a3c15ea073b511838b4e
JoshMLeslie <hello@joshmleslie>
2018-05-10 10:16:54 -0400

    Start NavBar creation

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
+import NavBarContainer from './navbar/navbar_container';
+
 const App = () => (
   <div>
-     <header>
-       <GreetingContainer />
-     </header>
-
-     <Route path="/login" component={SessionFormContainer} />
-     <Route path="/signup" component={SessionFormContainer} />
+     <switch>
+       <Route path="/" component={NavBarContainer} />
+       <Route path="/login" component={SessionFormContainer} />
+       <Route path="/signup" component={SessionFormContainer} />
+     </switch>
    </div>
 );
 
---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class NavBar extends React.Component {
+
+
+  render(){
+    <h1>rendering navbar<h1>
+  }
+}
+
+export default withRouter(NavBar);
