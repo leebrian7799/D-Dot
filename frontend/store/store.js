909f3f122785b87d32156b321465fe6f7cd02a28
Brian Lee <leebrian7799@gmail.com>
2018-05-14 17:22:11 -0400

    fix npm issues again

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,14 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = (preloadedState = {}) => {
   return (
     createStore(
       rootReducer,
       preloadedState,
-      composeWithDevTools(applyMiddleware(thunk, logger))
+      applyMiddleware(thunk, logger)
     )
   );
 };
