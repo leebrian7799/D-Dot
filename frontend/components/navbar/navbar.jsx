60b457a714e98cdc592f635b4c1a7ba5c3a5e4ab
Brian Lee <leebrian7799@gmail.com>
2018-05-14 03:37:40 -0400

    Modal

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,6 +2,38 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import GreetingContainer from '../greeting/greeting_container';
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen: false };
+
+    this.handleModal = this.handleModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+    }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({ modalOpen: false });
+  }
+
+
+  handleModal(boolean = false) {
+    this.setState({modalOpen: boolean});
+  }
+
+  openModal() {
+    this.setState({modalOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalOpen: false});
+  }
+
+  handleLogOut(e) {
+    e.preventDefault();
+    this.props.logout().then(() => this.props.history.push('/'));
+  }
+
   render(){
     return (
       <div className="navbar">
